529e5953463f35071c76c184b7f328ab87849155
João Russo <r0u6s12s18o@gmail.com>
2025-03-28 01:07:40 +0000

    feat: do problem 1 part 3

--- src/codyssi/sample_2024/problem1.rs
+++ src/codyssi/sample_2024/problem1.rs
@@ -2,11 +2,24 @@ pub fn run() {
 
     let input = std::fs::read_to_string("input/codyssi/sample_2024/problem1.txt")
         .unwrap();
+
     let mut prices = input.lines()
-        .map(|line| line.parse::<u64>().unwrap())
-        .collect::<Vec<u64>>();
+        .map(|line| line.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+
+    let discount = prices.iter()
+        .enumerate()
+        .map(|(i, a)| {
+            match i % 2 == 0 {
+                true => *a as i64,
+                false => -(*a as i64),
+            }
+        })
+        .sum::<i64>();
+
     prices.sort_by(|a, b| b.cmp(a));
 
-    println!("Part 1: {}", prices.iter().sum::<u64>());
-    println!("Part 2: {}", prices.iter().skip(20).sum::<u64>());
+    println!("Part 1: {}", prices.iter().sum::<u32>());
+    println!("Part 2: {}", prices.iter().skip(20).sum::<u32>());
+    println!("Part 3: {}", discount);
 }
