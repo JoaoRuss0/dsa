b7866dc63dd6c0bbf949b2e40a005778b068010d
João Russo <r0u6s12s18o@gmail.com>
2025-03-29 00:33:43 +0000

    feat: do problem 2

---
+++ src/codyssi/sample_2024/problem2.rs
@@ -0,0 +1,54 @@
+pub fn run() {
+
+    println!("Problem 2");
+
+    let input = std::fs::read_to_string("input/codyssi/sample_2024/problem2.txt")
+        .unwrap();
+    
+    let sensors = input.lines()
+        .map(|line| line == "TRUE")
+        .collect::<Vec<bool>>();
+
+    let true_sensor_ids_sum = sensors.iter()
+        .filter(|&s| *s)
+        .enumerate()
+        .map(|(i, _)| i + 1)
+        .sum::<usize>();
+
+    println!("- Part 1: {}", true_sensor_ids_sum);
+
+    let get_gate_output = |(i, inputs): (usize, &[bool])| -> bool {
+        match i % 2 == 0{
+            true => inputs[0] && inputs[1],
+            false =>  inputs[0] || inputs[1]
+        }
+    };
+
+    let true_gate_output_count = sensors
+        .chunks(2)
+        .enumerate()
+        .map(get_gate_output)
+        .filter(|&x| x)
+        .count();
+
+    println!("- Part 2: {}", true_gate_output_count);
+
+    let mut true_gate_output_layer_count = 0;
+    let mut layer = sensors.clone();
+
+    while layer.len() != 1 {
+
+        let next_layer = layer.chunks(2)
+            .enumerate()
+            .map(get_gate_output)
+            .collect::<Vec<bool>>();
+
+        true_gate_output_layer_count += next_layer.iter()
+            .filter(|&i| *i )
+            .count();
+
+        layer = next_layer;
+    }
+
+    println!("- Part 3: {}", true_gate_output_layer_count + input.lines().filter(|line| *line == "TRUE").count());
+}
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod codyssi;
 
 fn main() {
     codyssi::sample_2024::problem1::run();
+    codyssi::sample_2024::problem2::run();
 }
