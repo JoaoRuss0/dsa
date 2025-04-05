7b3b89dd742c1bb59ca887b108bebd91e0afd53a
João Russo <r0u6s12s18o@gmail.com>
2025-04-05 03:07:09 +0100

    feat: improve on leetcode 217

--- src/leetcode/E217.rs
+++ src/leetcode/E217.rs
@@ -1,10 +1,17 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 struct Solution;
 
 impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        nums.iter().collect::<HashSet<&i32>>().len() < nums.len()
+        let mut map: HashMap<i32, usize> = HashMap::new();
+        for x in nums {
+            if map.contains_key(&x) {
+                return true;
+            }
+            map.insert(x, map.len());
+        }
+        false
     }
 }
 
