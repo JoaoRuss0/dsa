c37a3e5f24c50605183333dacd6d934750d322e1
João Russo <r0u6s12s18o@gmail.com>
2025-04-20 22:45:27 +0100

    feat: do leetcode 26

---
+++ src/leetcode/E26.rs
@@ -0,0 +1,28 @@
+struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut i = 1;
+        let mut j = 1;
+        while j < nums.len() {
+            if nums[i - 1] != nums[j] {
+                nums[i] = nums[j];
+                i += 1;
+            }
+            j += 1;
+        }
+
+        i as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut nums = vec![1, 1, 2, 2, 3, 4, 5];
+        assert_eq!(5, Solution::remove_duplicates(&mut nums));
+    }
+}
