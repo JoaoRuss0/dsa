e4af7b7722a22c7913b6bd45b0778e25956f2846
João Russo <r0u6s12s18o@gmail.com>
2025-05-03 17:27:37 +0100

    improve: use a stack on problem 496

--- src/leetcode/E496.rs
+++ src/leetcode/E496.rs
@@ -1,36 +1,24 @@
+use std::collections::HashMap;
+
 struct Solution;
 
 impl Solution {
     pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-        let mut greater: Vec<i32> = Vec::new();
+        let index_of: HashMap<i32, usize> =
+            nums1.iter().enumerate().map(|(i, &j)| (j, i)).collect();
 
-        for i in nums1 {
-            let mut index_of = nums2.len();
-            for (index, &j) in nums2.iter().enumerate() {
-                if j == i {
-                    index_of = index;
-                    break;
-                }
+        let mut stack = Vec::new();
+        let mut greater: Vec<i32> = vec![-1; nums1.len()];
+
+        nums2.iter().for_each(|&j| {
+            while !stack.is_empty() && stack.last().is_some() && stack.last().unwrap() < &j {
+                greater[index_of[&stack.pop().unwrap()]] = j;
             }
 
-            if index_of == nums2.len() {
-                continue;
+            if index_of.contains_key(&j) {
+                stack.push(j);
             }
-
-            let mut found = false;
-
-            for &k in nums2[index_of + 1..].iter() {
-                if k > i {
-                    greater.push(k);
-                    found = true;
-                    break;
-                }
-            }
-
-            if !found {
-                greater.push(-1);
-            }
-        }
+        });
 
         greater
     }
@@ -45,6 +33,17 @@ mod tests {
         assert_eq!(
             vec![-1, 3, -1],
             Solution::next_greater_element(vec![4, 1, 2], vec![1, 3, 4, 2])
+        );
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            vec![5, 2, 6, 9, 11, 3, 7],
+            Solution::next_greater_element(
+                vec![3, 1, 5, 7, 9, 2, 6],
+                vec![1, 2, 3, 5, 6, 7, 9, 11]
+            )
         );
     }
 }
