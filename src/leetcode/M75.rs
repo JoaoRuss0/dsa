5c2bdb451a053d22c3c48afec7c6774d47a700b8
João Russo <r0u6s12s18o@gmail.com>
2025-05-06 22:54:31 +0100

    feat: use dutch natial flag problem algorithm on 75

--- src/leetcode/M75.rs
+++ src/leetcode/M75.rs
@@ -2,26 +2,22 @@ struct Solution;
 
 impl Solution {
     pub fn sort_colors(nums: &mut [i32]) {
-        let mut sorted = false;
-        while !sorted {
-            let mut switched = false;
+        let mut left = 0;
+        let mut right = nums.len() as i32 - 1;
 
-            let mut i = 0;
-            while i < nums.len() - 1 {
-                let current = nums[i];
-                let next = nums[i + 1];
-
-                if current > next {
-                    nums[i] = next;
-                    nums[i + 1] = current;
-                    switched = true;
+        let mut i = 0;
+        while right >= 0 && i <= right as usize {
+            match nums[i] {
+                0 => {
+                    nums.swap(i, left);
+                    left += 1;
+                    i += 1;
                 }
-
-                i += 1;
-            }
-
-            if !switched {
-                sorted = true;
+                2 => {
+                    nums.swap(i, right as usize);
+                    right -= 1;
+                }
+                _ => i += 1,
             }
         }
     }
@@ -43,5 +39,26 @@ mod tests {
         let mut colors = vec![2, 0, 2, 1, 1, 0];
         Solution::sort_colors(&mut colors);
         assert_eq!(vec![0, 0, 1, 1, 2, 2], colors);
+    }
+
+    #[test]
+    fn test3() {
+        let mut colors = vec![0, 2, 0, 1];
+        Solution::sort_colors(&mut colors);
+        assert_eq!(vec![0, 0, 1, 2], colors);
+    }
+
+    #[test]
+    fn test4() {
+        let mut colors = vec![2];
+        Solution::sort_colors(&mut colors);
+        assert_eq!(vec![2], colors);
+    }
+
+    #[test]
+    fn test5() {
+        let mut colors = vec![2, 2];
+        Solution::sort_colors(&mut colors);
+        assert_eq!(vec![2, 2], colors);
     }
 }
