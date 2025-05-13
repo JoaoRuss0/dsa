7abce1acd89cfbb115a3f34a2a2ec601add0a13a
João Russo <r0u6s12s18o@gmail.com>
2025-05-13 13:34:25 +0100

    improvement: add the number of ways to get to the above and left positions instead on 62

--- src/leetcode/M62.rs
+++ src/leetcode/M62.rs
@@ -1,41 +1,19 @@
-use std::collections::HashMap;
-
 struct Solution;
 
 impl Solution {
     pub fn unique_paths(m: i32, n: i32) -> i32 {
-        let mut layer = HashMap::new();
-        layer.insert((0, 0), 1);
+        let m = m as usize;
+        let n = n as usize;
+        let mut grid = vec![vec![1; n]; m];
 
-        let directions = vec![(0, 1), (1, 0)];
-        let mut unique = 0;
-
-        while !layer.is_empty() {
-            let mut next_layer = HashMap::new();
-            for (&(x, y), count) in layer.iter() {
-                if (x, y) == (m - 1, n - 1) {
-                    unique += count;
-                    continue;
-                }
-
-                for (dx, dy) in &directions {
-                    let next = (x + dx, y + dy);
-                    if !is_position_within_grid(next, (m, n)) {
-                        continue;
-                    }
-
-                    *next_layer.entry(next).or_insert(0) += count;
-                }
+        for x in 1..m {
+            for y in 1..n {
+                grid[x][y] = grid[x - 1][y] + grid[x][y - 1];
             }
-            layer = next_layer;
         }
 
-        unique
+        grid[m - 1][n - 1]
     }
-}
-
-fn is_position_within_grid((x, y): (i32, i32), (mx, my): (i32, i32)) -> bool {
-    x >= 0 && y >= 0 && x < mx && y < my
 }
 
 #[cfg(test)]
