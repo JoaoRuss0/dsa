811799e66fb3ff8207cb3a2e3a10d192e5c4d505
João Russo <r0u6s12s18o@gmail.com>
2025-05-31 04:25:50 +0100

    revert: remove unintentional main change

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
-mod advent_of_code;
 mod codyssi;
 mod everybody_codes;
 mod leetcode;
 
 fn main() {
-    //codyssi::run::execute()
-    //everybody_codes::run::execute()
-    advent_of_code::run::execute();
+    //codyssi::run::execute();
+    everybody_codes::run::execute();
 }
