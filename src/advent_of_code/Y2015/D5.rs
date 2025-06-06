ecd141615b2edbc6338d58b4446c678d47265184
João Russo <r0u6s12s18o@gmail.com>
2025-06-06 23:56:33 +0100

    feat: do a first iteration of advent of code 2015 day 5 part 2

--- src/advent_of_code/Y2015/D5.rs
+++ src/advent_of_code/Y2015/D5.rs
@@ -9,31 +9,51 @@ pub fn run() {
     let path = "input/advent_of_code/Y2015/D5.txt";
     let input = std::fs::read_to_string(path).unwrap();
 
-    let valid = input
-        .lines()
-        .map(|w| {
-            let mut two = HashSet::new();
-            let mut vowels = 0;
-            let mut double = false;
+    let niceness = input.lines().map(is_nice).collect::<Vec<(bool, bool)>>();
+    println!(
+        "  │  ├─ Part 1: {}",
+        niceness.iter().filter(|&&(one, _)| one).count()
+    );
+    println!(
+        "  │  └─ Part 2: {}",
+        niceness.iter().filter(|&&(_, two)| two).count()
+    );
+}
 
-            let chars = w.chars().collect::<Vec<char>>();
-            if VOWELS.contains(&chars[0]) {
-                vowels += 1;
+fn is_nice(word: &str) -> (bool, bool) {
+    let mut two = HashSet::new();
+    let mut vowels = 0;
+    let mut double = false;
+
+    let mut twice = Vec::with_capacity(word.len() - 1);
+    let mut is_twice = false;
+    let mut xyx = false;
+
+    let chars = word.chars().collect::<Vec<char>>();
+
+    for i in 0..word.len() {
+        if i >= 1 {
+            let cc = &word[i - 1..i + 1];
+            two.insert(cc);
+            double |= chars[i] == chars[i - 1];
+
+            if !is_twice {
+                is_twice |= twice[0..i - 1].contains(&cc);
+                twice.push(cc);
             }
+        }
+        if VOWELS.contains(&chars[i]) {
+            vowels += 1;
+        }
 
-            for i in 1..w.len() {
-                two.insert(&w[i - 1..i + 1]);
-                double |= chars[i] == chars[i - 1];
-                if VOWELS.contains(&chars[i]) {
-                    vowels += 1;
-                }
-            }
+        if i >= 2 && !xyx {
+            let aba = &chars[i - 2..i + 1];
+            xyx |= aba[0] == aba[2];
+        }
+    }
 
-            !INVALID_DOUBLE_CHARS.iter().any(|&s| two.contains(s)) && vowels >= 3 && double
-        })
-        .filter(|&x| x)
-        .count();
-
-    println!("  │  ├─ Part 1: {}", valid);
-    //println!("  │  └─ Part 2: {}", );
+    (
+        !INVALID_DOUBLE_CHARS.iter().any(|&s| two.contains(s)) && vowels >= 3 && double,
+        is_twice && xyx,
+    )
 }
