ee83135c653b55bcf14a2a7f24f25ea505e62da4
João Russo <r0u6s12s18o@gmail.com>
2025-06-24 22:36:31 +0100

    feat: do advent of code 2016 day 1 part 2

--- src/advent_of_code/Y2016/D1.rs
+++ src/advent_of_code/Y2016/D1.rs
@@ -1,44 +1,157 @@
+use crate::advent_of_code::Y2016::D1::Direction::*;
+use std::collections::HashSet;
+use std::fmt::Display;
+
 pub fn run() {
     println!("  ├─ Day 1 - No Time for a Taxicab");
 
     let path = "input/advent_of_code/Y2016/D1.txt";
     let input = std::fs::read_to_string(path).unwrap();
+    let mut instructions = get_instructions(&input);
 
-    let instructions = input
+    let mut p = Pose {
+        pos: Position::new(),
+        dir: Up,
+    };
+
+    let mut twice: Option<Position> = None;
+    let mut seen = HashSet::new();
+    seen.insert(p.pos);
+
+    for i in instructions {
+        let positions = p.apply(i);
+        positions.into_iter().for_each(|p| {
+            if twice.is_none() {
+                match seen.get(&p) {
+                    None => {
+                        seen.insert(p);
+                    }
+                    Some(_) => twice = Some(p),
+                }
+            }
+        });
+    }
+
+    println!("  │  ├─ Part 1: {}", p.pos.x.abs() + p.pos.y.abs());
+    let unwrapped = twice.unwrap();
+    println!("  │  └─ Part 2: {}", unwrapped.x.abs() + unwrapped.y.abs());
+}
+
+#[derive(Copy, Clone)]
+enum Rotation {
+    Left,
+    Right,
+}
+
+impl Rotation {
+    fn from_char(c: char) -> Self {
+        match c {
+            'L' => Rotation::Left,
+            'R' => Rotation::Right,
+            _ => panic!("Not a valid rotation"),
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn offset(self) -> (i32, i32) {
+        match self {
+            Up => (1, 0),
+            Down => (-1, 0),
+            Left => (0, -1),
+            Right => (0, 1),
+        }
+    }
+    fn rotate(self, r: Rotation) -> Direction {
+        match r {
+            Rotation::Left => match self {
+                Up => Left,
+                Right => Up,
+                Down => Right,
+                Left => Down,
+            },
+            Rotation::Right => match self {
+                Up => Right,
+                Right => Down,
+                Down => Left,
+                Left => Up,
+            },
+        }
+    }
+}
+
+struct Instruction {
+    rotation: Rotation,
+    times: usize,
+}
+
+#[derive(Copy, Clone)]
+struct Pose {
+    pos: Position,
+    dir: Direction,
+}
+
+impl Pose {
+    fn apply(&mut self, i: Instruction) -> Vec<Position> {
+        let d = self.dir.rotate(i.rotation);
+        let offset = d.offset();
+
+        self.dir = d;
+        self.pos.add(offset, i.times)
+    }
+}
+
+#[derive(Eq, PartialEq, Hash, Clone, Copy)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+impl Position {
+    fn new() -> Position {
+        Position { x: 0, y: 0 }
+    }
+
+    fn add(&mut self, offset: (i32, i32), times: usize) -> Vec<Position> {
+        let (x, y) = offset;
+
+        let mut positions = vec![];
+
+        for _ in 0..times {
+            self.x += x;
+            self.y += y;
+            positions.push(self.clone());
+        }
+
+        positions
+    }
+}
+
+fn get_instructions(input: &str) -> Vec<Instruction> {
+    input
         .lines()
         .next()
         .unwrap()
         .split(", ")
         .map(|s| {
-            let mut chars = s.chars().collect::<Vec<char>>();
-            let mut num = chars[1..]
+            let chars = s.chars().collect::<Vec<char>>();
+            let num = chars[1..]
                 .iter()
                 .collect::<String>()
-                .parse::<u32>()
+                .parse::<usize>()
                 .unwrap();
 
-            (chars[0], num)
+            Instruction {
+                rotation: Rotation::from_char(chars[0]),
+                times: num,
+            }
         })
-        .collect::<Vec<(char, u32)>>();
-
-    let mut directions: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
-    let mut direction_idx: usize = 0;
-    let mut pos = (0i32, 0i32);
-    instructions.iter().for_each(|&(dir, num)| {
-        direction_idx = (direction_idx as i32
-            + match dir {
-                'R' => 1,
-                'L' => -1,
-                _ => panic!("Not a valid rotation"),
-            })
-        .rem_euclid(4) as usize;
-
-        pos = (
-            pos.0 + (directions[direction_idx].0 * num as i32),
-            pos.1 + (directions[direction_idx].1 * num as i32),
-        );
-    });
-
-    println!("  │  ├─ Part 1: {}", pos.0.abs() + pos.1.abs());
-    //println!("  │  └─ Part 2: {}", );
+        .collect::<Vec<Instruction>>()
 }
