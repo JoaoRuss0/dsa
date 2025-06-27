0271282bd79f91ce26b0dbcd5231785c6146882e
João Russo <r0u6s12s18o@gmail.com>
2025-06-27 19:27:02 +0100

    feat: do advent of code 2017 day 1 part 2

--- src/advent_of_code/Y2017/D1.rs
+++ src/advent_of_code/Y2017/D1.rs
@@ -9,14 +9,23 @@ pub fn run() {
         .map(|c| c.to_digit(10).unwrap())
         .collect::<Vec<u32>>();
 
-    println!("  │  ├─ Part 1: {}", sum(nums));
-    //println!("  │  └─ Part 2: {}", );
+    let get_next_index = |cur_idx: usize, nums: &Vec<u32>| -> usize { (cur_idx + 1) % nums.len() };
+
+    println!("  │  ├─ Part 1: {}", sum(&nums, get_next_index));
+
+    let get_next_halfway_around_index =
+        |cur_idx: usize, nums: &Vec<u32>| -> usize { (cur_idx + (nums.len() / 2)) % nums.len() };
+
+    println!(
+        "  │  └─ Part 2: {}",
+        sum(&nums, get_next_halfway_around_index)
+    );
 }
 
-fn sum(nums: Vec<u32>) -> u32 {
+fn sum(nums: &Vec<u32>, get_digit: fn(usize, &Vec<u32>) -> usize) -> u32 {
     let mut sum = 0;
     for i in 0..nums.len() {
-        if nums[i] == nums[(i + 1) % nums.len()] {
+        if nums[i] == nums[get_digit(i, nums)] {
             sum += nums[i];
         }
     }
