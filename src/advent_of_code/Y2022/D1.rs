3964d9a6ccffbbf7b471aebdd98f8839de344b7f
João Russo <r0u6s12s18o@gmail.com>
2025-07-02 22:11:27 +0100

    feat: do advent of code 2022 day 1 part 2

--- src/advent_of_code/Y2022/D1.rs
+++ src/advent_of_code/Y2022/D1.rs
@@ -5,22 +5,16 @@ pub fn run() {
     let input = std::fs::read_to_string(path).unwrap();
 
     let inventories = input.split("\n\n").collect::<Vec<&str>>();
-    let calories = inventories
+    let mut calories = inventories
         .iter()
-        .map(|i| {
-            i.lines()
-                .map(|l| l.parse::<u32>().unwrap())
-                .collect::<Vec<u32>>()
-        })
-        .collect::<Vec<Vec<u32>>>();
+        .map(|i| i.lines().map(|l| l.parse::<u32>().unwrap()).sum::<u32>())
+        .collect::<Vec<u32>>();
 
+    println!("  │  ├─ Part 1: {}", calories.iter().max().unwrap());
+
+    calories.sort_by(|a, b| b.cmp(&a));
     println!(
-        "  │  ├─ Part 1: {}",
-        calories
-            .iter()
-            .map(|c| c.iter().sum::<u32>())
-            .max()
-            .unwrap()
+        "  │  └─ Part 2: {}",
+        calories[0] + calories[1] + calories[2]
     );
-    //println!("  │  └─ Part 2: {}", );
 }
