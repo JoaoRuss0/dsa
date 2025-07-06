795ddab659efa4d198aa55be90f392cc5c0967ad
João Russo <r0u6s12s18o@gmail.com>
2025-07-06 22:37:08 +0100

    feat: do advent of code 2024 day 3 part 2

--- src/advent_of_code/Y2024/D3.rs
+++ src/advent_of_code/Y2024/D3.rs
@@ -6,20 +6,57 @@ pub fn run() {
     let path = "input/advent_of_code/Y2024/D3.txt";
     let input = std::fs::read_to_string(path).unwrap();
 
-    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let muls = regex
-        .captures_iter(&input)
-        .map(|captured| {
-            (
-                captured[1].parse::<u32>().unwrap(),
-                captured[2].parse::<u32>().unwrap(),
-            )
-        })
-        .collect::<Vec<(u32, u32)>>();
-
     println!(
         "  │  ├─ Part 1: {}",
-        muls.iter().map(|(m, n)| m * n).sum::<u32>()
+        parse(&input).iter().map(|(m, n)| m * n).sum::<u32>()
     );
-    //println!("  │  └─ Part 2: {}", );
+
+    println!(
+        "  │  └─ Part 2: {}",
+        parse_enabled(&input)
+            .iter()
+            .map(|(m, n)| m * n)
+            .sum::<u32>()
+    );
+}
+
+fn parse(input: &str) -> Vec<(u32, u32)> {
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    regex
+        .captures_iter(&input)
+        .map(|c| (c[1].parse::<u32>().unwrap(), c[2].parse::<u32>().unwrap()))
+        .collect()
+}
+
+fn parse_enabled(input: &str) -> Vec<(u32, u32)> {
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+
+    let mut instructions = Vec::new();
+    let mut enabled = true;
+
+    for captured in regex.captures_iter(&input) {
+        match captured[0].to_string().as_str() {
+            "don't()" => {
+                enabled = false;
+                continue;
+            }
+            "do()" => {
+                enabled = true;
+                continue;
+            }
+            _ => {
+                if !enabled {
+                    continue;
+                }
+            }
+        }
+
+        instructions.push((
+            captured[1].parse::<u32>().unwrap(),
+            captured[2].parse::<u32>().unwrap(),
+        ))
+    }
+
+    instructions
 }
