078e41a3df372dce09f195d8ad880f0285eca1e8
João Russo <r0u6s12s18o@gmail.com>
2026-01-14 22:04:01 +0000

    feat: do flip flop 2025 puzzle 2 part 2

--- src/flip_flop/Y2025/P2.rs
+++ src/flip_flop/Y2025/P2.rs
@@ -7,23 +7,41 @@ pub fn run() {
     let movements: Vec<char> = input.chars().collect();
     let peaks = calculate_peaks(movements);
 
-    println!("  │  ├─ Part 1: {}", peaks.iter().max().unwrap());
-    //println!("  │  ├─ Part 2: {}",);
+    println!(
+        "  │  ├─ Part 1: {}",
+        peaks.iter().map(|p| p.0).max().unwrap()
+    );
+    println!(
+        "  │  ├─ Part 2: {}",
+        peaks.iter().map(|p| p.1).max().unwrap()
+    );
     //println!("  │  └─ Part 3: {}",);
 }
 
-fn calculate_peaks(movements: Vec<char>) -> Vec<i64> {
+fn calculate_peaks(movements: Vec<char>) -> Vec<(i64, i64)> {
     let mut height = 0;
+    let mut higher_height = 0;
+
     let mut peaks = Vec::new();
 
+    let mut last = ('-', 1);
     for movement in movements {
-        height += match movement {
+        let amount = match movement {
             'v' => -1,
             '^' => 1,
             _ => panic!("Invalid movement: {}", movement),
         };
-        peaks.push(height);
-    }
 
+        let times = match last.0 == movement {
+            true => last.1 + 1,
+            false => 1,
+        };
+
+        height += amount;
+        higher_height += times * amount;
+
+        peaks.push((height, higher_height));
+        last = (movement, times);
+    }
     peaks
 }
