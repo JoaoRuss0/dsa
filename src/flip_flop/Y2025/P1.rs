4169d27d414eb80194bca10ffdfb8a493cf4388e
João Russo <r0u6s12s18o@gmail.com>
2026-01-14 00:27:32 +0000

    feat: do flip flop 2025 puzzle 1 part 2

--- src/flip_flop/Y2025/P1.rs
+++ src/flip_flop/Y2025/P1.rs
@@ -4,19 +4,51 @@ pub fn run() {
     let path = "input/flip_flop/Y2025/P1.txt";
     let input = std::fs::read_to_string(path).unwrap();
 
-    let ba_na_ne_s = input
+    let ba_na_ne_s: Vec<(usize, usize)> = input
         .lines()
-        .map(|line| {
-            line.chars()
-                .collect::<Vec<char>>()
-                .chunks(2)
-                .map(|chunk| chunk.iter().collect::<String>())
-                .collect::<Vec<String>>()
-        })
-        .flatten()
-        .filter(|pair| vec!["ba".to_string(), "na".to_string(), "ne".to_string()].contains(pair))
-        .count();
+        .enumerate()
+        .map(|(i, line)| (i, calculate_banana_score(line)))
+        .collect();
 
-    println!("  │  ├─ Part 1: {}", ba_na_ne_s);
-    //println!("  │  └─ Part 2: {}",);
+    println!(
+        "  │  ├─ Part 1: {}",
+        ba_na_ne_s.iter().map(|(_, score)| score).sum::<usize>()
+    );
+
+    println!(
+        "  │  └─ Part 2: {}",
+        ba_na_ne_s
+            .iter()
+            .filter(|(_, score)| score.is_multiple_of(2))
+            .map(|(_, score)| score)
+            .sum::<usize>()
+    );
+}
+
+pub fn calculate_banana_score(line: &str) -> usize {
+    let ba_na_ne_s: Vec<&str> = vec!["ba", "na", "ne"];
+
+    let mut score = 0;
+    let mut pair = vec![];
+    let mut odd = true;
+
+    for char in line.chars() {
+        if odd {
+            odd = false;
+            pair.push(char);
+            continue;
+        }
+        pair.push(char);
+
+        print!("{} ", pair.iter().collect::<String>());
+        if ba_na_ne_s.contains(&pair.iter().collect::<String>().as_str()) {
+            score += 1;
+        }
+
+        pair.clear();
+        odd = true;
+    }
+
+    println!("{}", score);
+    score
 }
