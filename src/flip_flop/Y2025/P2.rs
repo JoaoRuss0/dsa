f44012c24e13a54b541755607349fabd73822a55
João Russo <r0u6s12s18o@gmail.com>
2026-01-15 00:53:46 +0000

    feat: do flip flop 2025 puzzle 2 part 3

--- src/flip_flop/Y2025/P2.rs
+++ src/flip_flop/Y2025/P2.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn run() {
     println!("  ├─ Puzzle 2: Rollercoaster Heights");
 
@@ -15,33 +17,75 @@ pub fn run() {
         "  │  ├─ Part 2: {}",
         peaks.iter().map(|p| p.1).max().unwrap()
     );
-    //println!("  │  └─ Part 3: {}",);
+    println!(
+        "  │  └─ Part 3: {}",
+        peaks.iter().map(|p| p.2).max().unwrap()
+    );
 }
 
-fn calculate_peaks(movements: Vec<char>) -> Vec<(i64, i64)> {
-    let mut height = 0;
-    let mut higher_height = 0;
+fn calculate_peaks(movements: Vec<char>) -> Vec<(i64, i64, i64)> {
+    let mut height: i64 = 0;
+    let mut higher_height: i64 = 0;
+    let mut fib_score: i64 = 0;
+    let mut fib = Fibonacci::new();
 
-    let mut peaks = Vec::new();
+    let mut peaks: Vec<(i64, i64, i64)> = Vec::new();
 
-    let mut last = ('-', 1);
-    for movement in movements {
-        let amount = match movement {
-            'v' => -1,
-            '^' => 1,
-            _ => panic!("Invalid movement: {}", movement),
-        };
+    let get_signal = |m: char| match m {
+        'v' => -1,
+        '^' => 1,
+        _ => panic!("Invalid movement: {}", m),
+    };
 
-        let times = match last.0 == movement {
+    let mut last = (movements[0], 0);
+
+    for (i, &movement) in movements.iter().enumerate() {
+        let signal = get_signal(movement);
+        let times: i64 = match last.0 == movement {
             true => last.1 + 1,
             false => 1,
         };
 
-        height += amount;
-        higher_height += times * amount;
+        let to_add = match last.0 != movement {
+            true => get_signal(last.0) * fib.get(last.1 as u16) as i64,
+            false => 0,
+        };
 
-        peaks.push((height, higher_height));
+        height += signal;
+        higher_height += times * signal;
+        fib_score = fib_score.checked_add(to_add).unwrap();
+
+        peaks.push((height, higher_height, fib_score));
         last = (movement, times);
     }
     peaks
+}
+
+struct Fibonacci {
+    cache: HashMap<u16, u32>,
+}
+
+impl Fibonacci {
+    fn new() -> Self {
+        let mut f = Self {
+            cache: HashMap::new(),
+        };
+
+        f.cache.insert(1, 1);
+        f.cache.insert(2, 1);
+
+        f
+    }
+
+    fn get(&mut self, value: u16) -> u32 {
+        if self.cache.contains_key(&value) {
+            return self.cache[&value];
+        }
+
+        let prev = self.get(value - 1);
+        let prev_prev = self.get(value - 2);
+
+        self.cache.insert(value, prev + prev_prev);
+        self.cache[&value]
+    }
 }
