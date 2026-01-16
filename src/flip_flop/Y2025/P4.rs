cd3eb909ad455c4766d9b420516a77d196f2de4e
João Russo <r0u6s12s18o@gmail.com>
2026-01-16 01:17:51 +0000

    feat: update flip flop 2025 puzzle 4 part 2

--- src/flip_flop/Y2025/P4.rs
+++ src/flip_flop/Y2025/P4.rs
@@ -8,36 +8,54 @@ pub fn run() {
         .lines()
         .map(|l| {
             let split = l.split_once(",").unwrap();
-            (
-                split.0.parse::<u8>().unwrap(),
-                split.1.parse::<u8>().unwrap(),
-            )
+            Point {
+                x: split.0.parse::<u8>().unwrap(),
+                y: split.1.parse::<u8>().unwrap(),
+            }
         })
-        .collect::<Vec<(u8, u8)>>();
+        .collect::<Vec<Point>>();
 
-    let mut steps: u64 = 0;
-    let mut last = (0, 0);
-    for t in &trash {
-        steps += (t.0.abs_diff(last.0) + t.1.abs_diff(last.1)) as u64;
-        last = t.clone();
-    }
-
+    let mut steps = walk(&trash, |x, y| x.step(y));
     println!("  │  ├─ Part 1: {}", steps);
 
-    let mut steps: u64 = 0;
-    let mut last = (0, 0);
-    for t in trash {
-        let vector = (t.0.abs_diff(last.0), t.1.abs_diff(last.1));
-        let diff = vector.0.abs_diff(vector.1);
+    steps = walk(&trash, |x, y| x.step_diagonally(y));
+    println!("  │  ├─ Part 2: {}", steps);
 
-        match diff == 0 {
-            true => steps += vector.0 as u64,
-            false => steps += diff as u64 + vector.0.min(vector.1) as u64,
-        }
+    //println!("  │  └─ Part 3: {}",);
+}
 
-        last = t
+struct Point {
+    x: u8,
+    y: u8,
+}
+
+impl Point {
+    fn step(&self, next: &Point) -> u8 {
+        self.x.abs_diff(next.x) + self.y.abs_diff(next.y)
     }
 
-    println!("  │  ├─ Part 2: {}", steps);
-    //println!("  │  └─ Part 3: {}",);
+    fn step_diagonally(&self, next: &Point) -> u8 {
+        let vector = Point {
+            x: self.x.abs_diff(next.x),
+            y: self.y.abs_diff(next.y),
+        };
+        let diff = vector.x.abs_diff(vector.y);
+
+        match diff == 0 {
+            true => vector.x,
+            false => diff + vector.x.min(vector.y),
+        }
+    }
+}
+
+fn walk(trash: &Vec<Point>, count_steps: fn(&Point, &Point) -> u8) -> u64 {
+    let mut steps: u64 = 0;
+    let mut last = &Point { x: 0, y: 0 };
+
+    for t in trash {
+        steps += count_steps(t, last) as u64;
+        last = t;
+    }
+
+    steps
 }
