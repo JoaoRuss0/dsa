de8d81e225667fcae232883bebc3c5122ecef4c1
João Russo <r0u6s12s18o@gmail.com>
2026-01-16 01:04:01 +0000

    feat: do flip flop 2025 puzzle 4 part 2

--- src/flip_flop/Y2025/P4.rs
+++ src/flip_flop/Y2025/P4.rs
@@ -17,12 +17,27 @@ pub fn run() {
 
     let mut steps: u64 = 0;
     let mut last = (0, 0);
-    for t in trash {
+    for t in &trash {
         steps += (t.0.abs_diff(last.0) + t.1.abs_diff(last.1)) as u64;
-        last = t
+        last = t.clone();
     }
 
     println!("  │  ├─ Part 1: {}", steps);
-    //println!("  │  ├─ Part 2: {}",);
+
+    let mut steps: u64 = 0;
+    let mut last = (0, 0);
+    for t in trash {
+        let vector = (t.0.abs_diff(last.0), t.1.abs_diff(last.1));
+        let diff = vector.0.abs_diff(vector.1);
+
+        match diff == 0 {
+            true => steps += vector.0 as u64,
+            false => steps += diff as u64 + vector.0.min(vector.1) as u64,
+        }
+
+        last = t
+    }
+
+    println!("  │  ├─ Part 2: {}", steps);
     //println!("  │  └─ Part 3: {}",);
 }
