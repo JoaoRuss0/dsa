04f3af96661d325387212e72db4034bea9e7cafd
João Russo <r0u6s12s18o@gmail.com>
2026-01-17 03:18:41 +0000

    feat: do flip flop 2025 puzzle 5 part 2

--- src/flip_flop/Y2025/P5.rs
+++ src/flip_flop/Y2025/P5.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub fn run() {
     println!("  ├─ Puzzle 5: Strange Tunnels");
@@ -10,8 +10,10 @@ pub fn run() {
     let mut tunnels: HashMap<char, Tunnel> = HashMap::new();
     build_network(&mut tunnels, &sequence);
 
-    println!("  │  ├─ Part 1: {}", walk(&sequence, &tunnels));
-    //println!("  │  ├─ Part 2: {}",);
+    let (steps, unvisited) = walk(&sequence, &tunnels);
+
+    println!("  │  ├─ Part 1: {}", steps);
+    println!("  │  ├─ Part 2: {}", unvisited.iter().collect::<String>());
     //println!("  │  └─ Part 3: {}",);
 }
 
@@ -71,15 +73,33 @@ fn build_network(tunnels: &mut HashMap<char, Tunnel>, sequence: &[char]) {
     }
 }
 
-fn walk(sequence: &[char], tunnels: &HashMap<char, Tunnel>) -> usize {
+fn walk(sequence: &[char], tunnels: &HashMap<char, Tunnel>) -> (usize, Vec<char>) {
     let mut steps = 0;
     let mut next = 0;
 
+    let mut visited: HashSet<char> = HashSet::new();
+
     while next < sequence.len() {
+        visited.insert(sequence[next]);
         let tunnel = tunnels.get(&sequence[next]).unwrap();
         next = tunnel.exit(next) + 1;
         steps += tunnel.length;
     }
 
-    steps
+    (steps, get_ordered_difference(sequence, &visited))
+}
+
+fn get_ordered_difference(A: &[char], B: &HashSet<char>) -> Vec<char> {
+    let mut difference: Vec<char> = Vec::new();
+
+    let mut added = HashSet::new();
+
+    for c in A {
+        if !B.contains(c) && !added.contains(c) {
+            difference.push(*c);
+            added.insert(*c);
+        }
+    }
+
+    difference
 }
