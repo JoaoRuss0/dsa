b5aa4d0e935def18f2ad65c5e5b75483872b5622
João Russo <r0u6s12s18o@gmail.com>
2026-01-18 23:49:33 +0000

    feat: do flip flop 2025 puzzle 7 part 2

--- src/flip_flop/Y2025/P7.rs
+++ src/flip_flop/Y2025/P7.rs
@@ -1,4 +1,5 @@
 use crate::flip_flop::Y2025::P7::Direction::*;
+use crate::flip_flop::Y2025::P7::Direction3D::*;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::hash::{Hash, Hasher};
 
@@ -19,7 +20,22 @@ pub fn run() {
             })
             .sum::<u16>()
     );
-    //println!("  │  ├─ Part 2: {}",);
+
+    println!(
+        "  │  ├─ Part 2: {}",
+        input
+            .lines()
+            .map(|l| {
+                let split = l.split_once(" ").unwrap();
+                let grid = Grid3D::new(
+                    split.0.parse().unwrap(),
+                    split.1.parse().unwrap(),
+                    split.0.parse().unwrap(),
+                );
+                grid.find_all_shortest_paths()
+            })
+            .sum::<u64>()
+    );
     //println!("  │  └─ Part 3: {}",);
 }
 
@@ -201,6 +217,217 @@ impl From<Direction> for Vector {
             West => Vector { x: 0, y: 1 },
             South => Vector { x: -1, y: 0 },
             East => Vector { x: 0, y: -1 },
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Hash, Copy, Clone)]
+struct Point3D {
+    x: i16,
+    y: i16,
+    z: i16,
+}
+
+impl Point3D {
+    fn move_in_all_directions(&self) -> Vec<Self> {
+        Direction3D::ALL
+            .iter()
+            .map(|&d| self.move_in_direction(d))
+            .filter_map(|p| p)
+            .collect()
+    }
+
+    fn move_in_direction(&self, direction: Direction3D) -> Option<Self> {
+        let v: Vector3D = direction.into();
+
+        let n_x = self.x.checked_add(v.x);
+        let n_y = self.y.checked_add(v.y);
+        let n_z = self.z.checked_add(v.z);
+
+        if n_x.is_none() || n_y.is_none() || n_z.is_none() {
+            return None;
+        }
+
+        Some(Self {
+            x: n_x.unwrap(),
+            y: n_y.unwrap(),
+            z: n_z.unwrap(),
+        })
+    }
+}
+
+#[derive(Copy, Clone, Eq)]
+struct Path3D {
+    point: Point3D,
+    length: u64,
+}
+
+impl Path3D {
+    fn new(point: Point3D, length: u64) -> Self {
+        Self { point, length }
+    }
+}
+
+impl Hash for Path3D {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.point.hash(state);
+    }
+}
+
+impl PartialEq for Path3D {
+    fn eq(&self, other: &Self) -> bool {
+        self.point == other.point
+    }
+}
+
+struct Queue {
+    queue: VecDeque<Path3D>,
+    queued: HashMap<Path3D, u64>,
+}
+
+impl Queue {
+    fn new() -> Self {
+        Self {
+            queue: VecDeque::new(),
+            queued: HashMap::new(),
+        }
+    }
+
+    fn push(&mut self, path: Path3D, amount: u64) {
+        if self.queued.contains_key(&path) {
+            *self.queued.get_mut(&path).unwrap() += amount;
+            return;
+        }
+        self.queue.push_back(path);
+        self.queued.insert(path, amount);
+    }
+
+    fn pop(&mut self) -> Option<(Path3D, u64)> {
+        match self.queue.pop_front() {
+            Some(popped) => Some((popped, self.queued.remove(&popped).unwrap())),
+            None => None,
+        }
+    }
+
+    fn contains(&self, path: &Path3D) -> bool {
+        self.queued.contains_key(path)
+    }
+
+    fn prune(&mut self, length: u64) {
+        self.queue.retain(|p| p.length <= length);
+    }
+}
+
+struct Grid3D {
+    x_len: u8,
+    y_len: u8,
+    z_len: u8,
+    start: Point3D,
+    end: Point3D,
+}
+
+impl Grid3D {
+    fn new(x_len: u8, y_len: u8, z_len: u8) -> Self {
+        Self {
+            start: Point3D { x: 0, y: 0, z: 0 },
+            end: Point3D {
+                x: x_len as i16 - 1,
+                y: y_len as i16 - 1,
+                z: z_len as i16 - 1,
+            },
+            x_len,
+            y_len,
+            z_len,
+        }
+    }
+
+    fn find_all_shortest_paths(&self) -> u64 {
+        let mut visited = HashSet::new();
+        let mut length = None;
+        let mut paths = 0;
+
+        let mut queue = Queue::new();
+        queue.push(Path3D::new(self.start, 0), 1);
+
+        while let Some((path, amount)) = queue.pop() {
+            if visited.contains(&path) {
+                continue;
+            }
+            visited.insert(path);
+
+            if path.point == self.end {
+                match length {
+                    Some(l) => {
+                        if path.length == l {
+                            paths += amount;
+                        }
+                    }
+                    None => {
+                        length = Some(path.length);
+                        queue.prune(path.length);
+                        paths += amount;
+                    }
+                }
+                continue;
+            }
+
+            for new in path.point.move_in_all_directions() {
+                if self.is_out_of_bounds(new) {
+                    continue;
+                }
+
+                match length {
+                    Some(l) => {
+                        if path.length + 1 > l {
+                            continue;
+                        }
+                    }
+                    None => queue.push(Path3D::new(new, path.length + 1), amount),
+                }
+            }
+        }
+        paths
+    }
+
+    fn is_out_of_bounds(&self, point: Point3D) -> bool {
+        point.x > self.end.x
+            || point.x < 0
+            || point.y > self.end.y
+            || point.y < 0
+            || point.z > self.end.z
+            || point.z < 0
+    }
+}
+
+struct Vector3D {
+    x: i16,
+    y: i16,
+    z: i16,
+}
+
+#[derive(Copy, Clone)]
+enum Direction3D {
+    Up,
+    Right,
+    Down,
+    Left,
+    Front,
+    Back,
+}
+
+impl Direction3D {
+    const ALL: [Direction3D; 6] = [Up, Right, Down, Left, Front, Back];
+}
+
+impl From<Direction3D> for Vector3D {
+    fn from(value: Direction3D) -> Self {
+        match value {
+            Up => Vector3D { x: 0, y: 1, z: 0 },
+            Right => Vector3D { x: 1, y: 0, z: 0 },
+            Down => Vector3D { x: 0, y: -1, z: 0 },
+            Left => Vector3D { x: -1, y: 0, z: 0 },
+            Front => Vector3D { x: 0, y: 0, z: -1 },
+            Back => Vector3D { x: 0, y: 0, z: 1 },
         }
     }
 }
