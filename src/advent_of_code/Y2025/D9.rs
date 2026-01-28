29179663fb2a8d8f48134b4897f9912ed866d0cd
João Russo <r0u6s12s18o@gmail.com>
2026-01-28 10:45:29 +0000

    feat: do advent of code 2025 day 9

--- src/advent_of_code/Y2025/D9.rs
+++ src/advent_of_code/Y2025/D9.rs
@@ -1,5 +1,6 @@
 use std::collections::BTreeMap;
 use std::fmt::Formatter;
+use std::ops::Bound::Excluded;
 
 pub fn run() {
     println!("  ├─ Day 9 - Movie Theater");
@@ -11,50 +12,79 @@ pub fn run() {
 
     let mut x_edges = BTreeMap::new();
     let mut y_edges = BTreeMap::new();
-    for pair in red_tiles.windows(2) {
-        let x_1 = pair[0].x;
-        let x_2 = pair[1].x;
-        let y_1 = pair[0].y;
-        let y_2 = pair[1].y;
+
+    let mut add_edge = |t1: &Point, t2: &Point| {
+        let x_1 = t1.x;
+        let x_2 = t2.x;
+        let y_1 = t1.y;
+        let y_2 = t2.y;
 
         match x_1 != x_2 {
             true => y_edges
-                .entry(pair[0].y)
+                .entry(t1.y)
                 .or_insert(Vec::new())
                 .push((x_1.min(x_2), x_1.max(x_2))),
             false => x_edges
-                .entry(pair[0].x)
+                .entry(t1.x)
                 .or_insert(Vec::new())
                 .push((y_1.min(y_2), y_1.max(y_2))),
         }
-    }
+    };
+    red_tiles
+        .windows(2)
+        .for_each(|pair| add_edge(&pair[0], &pair[1]));
+    add_edge(&red_tiles[red_tiles.len() - 1], &red_tiles[0]);
 
-    let is_cut_by_any_edge =
-        |edge: (u64, u64), at: u64, opposite_edges: &BTreeMap<u64, Vec<(u64, u64)>>| -> bool {
-            let mut cut = false;
+    let rectangle_contains_edge = |(t1, t2): (&Point, &Point),
+                                   edges_at_an_x: &BTreeMap<u64, Vec<(u64, u64)>>,
+                                   edges_at_an_y: &BTreeMap<u64, Vec<(u64, u64)>>|
+     -> bool {
+        let (x_min, x_max) = (t1.x.min(t2.x), t1.x.max(t2.x));
+        let (y_min, y_max) = (t1.y.min(t2.y), t1.y.max(t2.y));
 
-            'outer: for i in edge.0..=edge.1 {
-                if let Some(edges) = opposite_edges.get(&i) {
-                    for &(s, e) in edges {
-                        match i == edge.0 || i == edge.1 {
-                            true => {
-                                if s < at && at < e {
-                                    cut = true;
-                                    break 'outer;
-                                }
-                            }
-                            false => {
-                                if s <= at && at <= e {
-                                    cut = true;
-                                    break 'outer;
-                                }
-                            }
+        if x_min + 1 >= x_max || y_min + 1 >= y_max {
+            return false;
+        }
+
+        let do_segments_overlap =
+            |s1: u64, e1: u64, s2: u64, e2: u64| -> bool { s1.max(s2) < e1.min(e2) };
+
+        let check_if_edges_cut_rectangle =
+            |value_start: u64,
+             value_end: u64,
+             perpendicular_edges_at_value: &BTreeMap<u64, Vec<(u64, u64)>>,
+             rectangle_start: u64,
+             rectangle_end: u64|
+             -> bool {
+                for (_, edges) in perpendicular_edges_at_value
+                    .range((Excluded(&value_start), Excluded(&value_end)))
+                {
+                    for &(start, end) in edges {
+                        if do_segments_overlap(start, end, rectangle_start, rectangle_end) {
+                            return true;
                         }
+                    }
+                }
+                false
+            };
+
+        check_if_edges_cut_rectangle(y_min, y_max, &edges_at_an_y, x_min, x_max)
+            || check_if_edges_cut_rectangle(x_min, x_max, &edges_at_an_x, y_min, y_max)
+    };
+
+    let point_is_inside_polygon =
+        |point: &Point, edges_at_an_x: &BTreeMap<u64, Vec<(u64, u64)>>| -> bool {
+            let mut intersect_edge_count = 0;
+            for (_, y_edges) in edges_at_an_x.range(0..=point.x) {
+                for &(start, end) in y_edges {
+                    if start <= point.y && point.y <= end {
+                        intersect_edge_count += 1;
+                        break;
                     }
                 }
             }
 
-            cut
+            intersect_edge_count % 2 == 1
         };
 
     let mut max_area = 0;
@@ -64,17 +94,15 @@ pub fn run() {
             let area = t1.area(t2);
             max_area = area.max(max_area);
 
-            let x_edge = (t1.x.min(t2.x), t1.x.max(t2.x));
-            let y_edge = (t1.y.min(t2.y), t1.y.max(t2.y));
-
-            if is_cut_by_any_edge(x_edge, t1.y, &x_edges)
-                || is_cut_by_any_edge(x_edge, t2.y, &x_edges)
-                || is_cut_by_any_edge(y_edge, t1.x, &y_edges)
-                || is_cut_by_any_edge(y_edge, t2.x, &y_edges)
+            if area <= max_bounded_area
+                || rectangle_contains_edge((t1, t2), &x_edges, &y_edges)
+                || !point_is_inside_polygon(t1, &x_edges)
             {
                 continue;
             }
-            max_bounded_area = max_bounded_area.max(area);
+
+            max_bounded_area = area;
+            println!("{t1} - {t2} = {area}");
         }
     }
 
