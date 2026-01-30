69ac9e5f190b866f45685d11905da9f6000ad138
João Russo <r0u6s12s18o@gmail.com>
2026-01-30 22:01:32 +0000

    feat: do advent of code 2025 day 11 part 1

---
+++ src/advent_of_code/Y2025/D11.rs
@@ -0,0 +1,37 @@
+use std::collections::{HashMap, VecDeque};
+
+pub fn run() {
+    println!("  ├─ Day 11 - Reactor");
+
+    let path = "input/advent_of_code/Y2025/D11.txt";
+    let input = std::fs::read_to_string(path).unwrap();
+
+    let racks = input
+        .lines()
+        .map(|l| {
+            let split = l.split(": ").collect::<Vec<&str>>();
+            let id = split[0];
+            let connections = split[1].split(" ").collect::<Vec<&str>>();
+            (id, connections)
+        })
+        .collect::<HashMap<&str, Vec<&str>>>();
+
+    let mut queue = VecDeque::new();
+    queue.push_back("you");
+
+    let mut paths = 0;
+
+    while let Some(next) = queue.pop_front() {
+        match next == "out" {
+            true => paths += 1,
+            false => racks
+                .get(&next)
+                .unwrap()
+                .iter()
+                .for_each(|&id| queue.push_back(id)),
+        }
+    }
+
+    println!("  │  ├─ Part 1: {}", paths);
+    //println!("  │  └─ Part 2: {}", );
+}
--- src/advent_of_code/Y2025/mod.rs
+++ src/advent_of_code/Y2025/mod.rs
@@ -1,5 +1,6 @@
 pub mod D1;
 pub mod D10;
+pub mod D11;
 pub mod D2;
 pub mod D3;
 pub mod D4;
