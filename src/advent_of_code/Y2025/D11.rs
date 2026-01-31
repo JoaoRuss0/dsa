7e2431e5ee5d7c1519f83eff2f9f7b58ec1f3a09
João Russo <r0u6s12s18o@gmail.com>
2026-01-31 01:20:05 +0000

    feat: update advent of code 2025 day 11 part 1

--- src/advent_of_code/Y2025/D11.rs
+++ src/advent_of_code/Y2025/D11.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
 pub fn run() {
     println!("  ├─ Day 11 - Reactor");
@@ -10,32 +10,60 @@ pub fn run() {
         .lines()
         .map(|l| {
             let split = l.split(": ").collect::<Vec<&str>>();
-            let id = split[0];
-            let connections = split[1].split(" ").collect::<Vec<&str>>();
+            let id = split[0].to_string();
+            let connections = split[1]
+                .split(" ")
+                .map(str::to_string)
+                .collect::<Vec<String>>();
             (id, connections)
         })
-        .collect::<HashMap<&str, Vec<&str>>>();
+        .collect::<HashMap<String, Vec<String>>>();
 
     println!("  │  ├─ Part 1: {}", dfs("you", "out", &racks));
     //println!("  │  └─ Part 2: {}", );
 }
 
-fn dfs(start: &str, end: &str, graph: &HashMap<&str, Vec<&str>>) -> usize {
-    let mut queue = VecDeque::new();
-    queue.push_back(start);
+fn dfs(start: &str, end: &str, graph: &HashMap<String, Vec<String>>) -> u64 {
+    let mut cache = HashMap::new();
 
-    let mut paths = 0;
+    let mut stack = Vec::new();
+    stack.push((start.to_string(), false));
 
-    while let Some(next) = queue.pop_front() {
-        match next == end {
-            true => paths += 1,
-            false => graph
-                .get(&next)
-                .unwrap()
-                .iter()
-                .for_each(|&id| queue.push_back(id)),
+    while let Some((curr, expanded)) = stack.pop() {
+        if cache.contains_key(&curr) {
+            continue;
+        }
+
+        if curr == end {
+            cache.insert(curr, 1);
+            continue;
+        }
+
+        match expanded {
+            false => {
+                stack.push((curr.clone(), true));
+                if let Some(next) = graph.get(&curr) {
+                    next.iter().for_each(|c| {
+                        if !cache.contains_key(c.as_str()) {
+                            stack.push((c.clone(), false));
+                        }
+                    })
+                }
+            }
+            true => {
+                let children_sum = graph
+                    .get(&curr)
+                    .map(|next| {
+                        next.iter()
+                            .map(|c| cache.get(c.as_str()).copied().unwrap_or(0))
+                            .sum::<u64>()
+                    })
+                    .unwrap_or(0);
+                cache.insert(curr, children_sum);
+            }
         }
     }
+    println!("{cache_hits}");
 
-    paths
+    *cache.get(start).unwrap()
 }
